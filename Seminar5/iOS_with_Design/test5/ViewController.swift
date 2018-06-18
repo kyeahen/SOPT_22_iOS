d604c03c86fe53b4893644ec27718bdc264eb6bf
kyeahen <bright19961@naver.com>
2018-06-18 20:18:20 +0900

    Add Seminar5

---
+++ Seminar5/iOS_with_Design/test5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test5
+//
+//  Created by 김예은 on 2018. 5. 19..
+//  Copyright © 2018년 yeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
